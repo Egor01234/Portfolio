e79efc7722d777fc8dc0ebe2fd48387e85119b14
Yehor Valesiuk <myemail@com>
2024-09-28 06:20:06 -0400

    Commit 4

--- portfolioproject/portfolio/component/Home.jsx
+++ portfolioproject/portfolio/component/Home.jsx
@@ -1,3 +1,10 @@
 export default function Home(){
-    return <p>Home</p>
+    return <div><p>I’m glad you’re here!
+         This portfolio showcases my work in AI Software Development field,
+          highlighting projects that reflect my creativity and skills. <br></br>
+           Each piece represents my passion for Software, 
+           and I hope it gives you a glimpse into my approach and vision. <br></br>
+           Feel free to explore, and I’d love to hear your thoughts or connect!
+     </p> <a href="/about" id = "AboutButton">About Me</a></div>
+    
 }
