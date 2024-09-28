bb884fefc8659d29ff76983322fd1305ca2a36a7
Yehor Valesiuk <myemail@com>
2024-09-28 06:47:28 -0400

    Commit 6.5

--- portfolioproject/portfolio/component/AboutMe.jsx
+++ portfolioproject/portfolio/component/AboutMe.jsx
@@ -1,11 +1,9 @@
 import image from '/PersonalPhoto.jpg'
 
 export default function AboutMe(){
-    return <div><p>•	Current student of Software Engineering Technology, specializing in Artificial Intelligence <br></br>
-        •	Completed multiple software development applied projects, demonstrating proficiencies with web development, creating interfaces, managing databases, using Agile practices and testing/debugging<br></br> 
-        •	Developing foundations of machine learning concepts, algorithms & data structures and AI ethics<br></br>
-        •	Effectively works both independently and within a collaborative team setting <br></br>
-        •	A passion and acumen for adapting to new environments and learning technologies 
+    return <div><p>My name is Yehor Valeiuk. I am Current student of Software Engineering Technology, specializing in Artificial Intelligence. 
+        I Completed multiple software development applied projects,  demonstrating proficiencies with web development, creating interfaces, managing databases, using Agile practices and testing/debugging. 
+        I continue studying with passion new technologies and develop myself.
         </p>
         <br></br>
         <img src={image} id = "image" />
